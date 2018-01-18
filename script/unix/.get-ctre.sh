21b55feecc9924b5caa0f823ff5af7f7e92cbc59
liamkinne <kinneliam@gmail.com>
2018-01-18 14:48:45 +1100

    made file names more universal

--- script/unix/.get-ctre.sh
+++ script/unix/.get-ctre.sh
@@ -2,8 +2,8 @@
 
 printf ".get_ctre: CTRE not found. Downloading..."
 wget -r -nd --progress=bar http://www.ctr-electronics.com//downloads/lib/CTRE_FRCLibs_NON-WINDOWS_v5.0.3.3.zip
-unzip -q CTRE_FRCLibs_NON-WINDOWS_v5.0.3.3.zip -d ctre_full
+unzip -q CTRE_FRCLibs_NON-WINDOWS_*.zip -d ctre_full
 printf ".get_ctre.sh: Cleaning up...\n"
 cp -r ctre_full/cpp CTRE
 rm -rf ctre_full
-rm CTRE_FRCLibs_NON-WINDOWS_v5.0.3.3.zip
+rm CTRE_FRCLibs_NON-WINDOWS_*.zip
